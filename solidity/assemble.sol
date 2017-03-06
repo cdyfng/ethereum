e895f35662cab929989efe14c16889d85c244528
cdyfng <xiaofeng_chen2007@163.com>
2017-03-06 11:56:20 +0800

    debug solid

---
+++ solidity/assemble.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.4.0;
+
+contract C {
+ function f(uint x) {
+    //assembly {
+    //  return(0, 0820)
+    //}
+    assembly { x := sub(x, 1) }
+  }
+
+  //function Base(bytes32) {(["a"]);}
+  function fest(uint[85678901] a) returns (bytes32) {}
+
+  function turpleadd() returns (uint a, uint b) {
+        //(a, b) += (1, 1);
+    }
+  uint x0;
+  function b() returns (uint) {
+      uint answer;
+
+      if(x0>3) answer = 42;
+      if(x0>3) answer = 42;
+      if(x0>3) answer = 42;
+      if(x0>3) answer = 42;
+      if(x0>3) answer = 42;
+      if(x0>3) answer = 42;
+      if(x0>3) answer = 42;
+      if(x0>3) answer = 42;
+      if(x0>3) answer = 42;
+      if(x0>3) answer = 42;
+      if(x0>3) answer = 42;
+      if(x0>3) answer = 42;
+      if(x0>3) answer = 42;
+      if(x0>3) answer = 42;
+
+      return answer;
+  }
+}
